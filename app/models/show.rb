d25af875c0656398624513ba92b417ef9480fb4a
teklumezgebo <mezge002@umn.edu>
2023-05-29 17:23:29 -0500

    commit tree a2866c7642e2eb53175431dc2e8ce53411634820

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def actors_list
+    list = []
+    self.actors.each do |actor|
+        list << "#{actor.first_name} #{actor.last_name}"
+    end
+    list
+  end
 end
